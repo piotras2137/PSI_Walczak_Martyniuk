171cc06c2d7762f92fed0e3df47c72060c6114c9
Piotr Martyniuk <piotras2137@gmail.com>
2021-10-19 19:48:09 +0200

    :

--- ćwiczenia/PiotrMartyniuk/lista2/zadanie1.py
+++ ćwiczenia/PiotrMartyniuk/lista2/zadanie1.py
@@ -1,5 +1,18 @@
-a_list = [i for i in range(1,10,1)]
+a_list = [i for i in range(10,0,-1)]
 b_list = [i for i in range(0,9,1)]
 
 print(a_list)
 print(b_list)
+x = 0 
+
+if len(a_list) > len(b_list):
+    x = len(a_list)
+else:
+    x = len(b_list)
+
+
+for i in range(0,x):
+    if i%2==0 and len(a_list) > i:
+        print(a_list[i])
+    elif len(b_list) > i and i%2!=0:
+        print(b_list[i])
