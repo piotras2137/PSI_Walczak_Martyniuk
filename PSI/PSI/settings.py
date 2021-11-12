e7e65bd82aefb8cc46ea212b37bb0c04e21c71c6
Piotr Martyniuk <piotras2137@gmail.com>
2021-11-12 11:43:46 +0100

    podpiecie pod baze mysql

--- PSI/PSI/settings.py
+++ PSI/PSI/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'PSI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'psiapi',
+        'USER': 'test',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
