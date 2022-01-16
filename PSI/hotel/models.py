2105cbe583b75fd640f69811e7ffb8f59e271035
Kacper Walczak <kacperski36@wp.pl>
2022-01-16 19:48:23 +0100

    Dodanie nowych filtrów wraz ze spersonalizowanymi filtrami do daty ceny czy ilosci lozek. Dodanie personalizowanych stringow w celu poprawy czytelnosci

--- PSI/hotel/models.py
+++ PSI/hotel/models.py
@@ -26,9 +26,15 @@ class Room(models.Model):
     day_price = models.IntegerField()
     bed_amount = models.IntegerField()
 
+    def __str__(self):
+        return str(self.pk)+', type: '+str(self.room_type)+', room number: '+str(self.room_number)+', day price: '+str(self.day_price)+', bed amount: '+str(self.bed_amount)
+
 
 class Reservation(models.Model):
     id_customer = models.ForeignKey(Customer, on_delete=CASCADE)
     id_room = models.ManyToManyField(Room)
     start_date = models.DateTimeField(auto_now=False)
     end_date = models.DateTimeField(auto_now=False)
+
+    def __str__(self):
+        return str(self.pk)+' '+str(self.id_customer)+' '+str(self.start_date)+' '+str(self.end_date)
--- PSI/hotel/views.py
+++ PSI/hotel/views.py
@@ -23,20 +23,24 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
+from django_filters import DateTimeFilter, NumberFilter, Filter, AllValuesFilter
+from django_filters.rest_framework import FilterSet
 
 
 import datetime
 
+
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
-        'customers': reverse('customers',request=request,format=format),
-        'customer list': reverse('customer list',request=request,format=format),
-        'rooms': reverse('rooms',request=request,format=format),
-        'room list': reverse('room list',request=request,format=format),
-        'reservations': reverse('reservations',request=request,format=format),
-        'reservation list': reverse('reservation list',request=request,format=format),
+        'customers': reverse('customers', request=request, format=format),
+        'customer list': reverse('customer list', request=request, format=format),
+        'rooms': reverse('rooms', request=request, format=format),
+        'room list': reverse('room list', request=request, format=format),
+        'reservations': reverse('reservations', request=request, format=format),
+        'reservation list': reverse('reservation list', request=request, format=format),
     })
+
 
 def index(request):
     """View for main page/index."""
@@ -70,10 +74,19 @@ class GenericCustomerList(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['first_name', 'second_name']
+    search_fields = ['first_name', 'second_name',
+                     'phone_number', 'email', 'personal_id']
+    ordering_fields = ['first_name', 'second_name',
+                       'phone_number']
+
 
 class Customerdetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CustomerSerializer
-    queryset = Customer.objects.all()  
+    queryset = Customer.objects.all()
+
 
 class Roomlist(APIView):
     def get(self, request, format=None):
@@ -88,37 +101,71 @@ class Roomlist(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class RoomFilter(FilterSet):
+    from_dayprice = NumberFilter(
+        field_name='day_price', lookup_expr='gte')
+    to_dayprice = NumberFilter(
+        field_name='day_price', lookup_expr='lte')
+    from_bedamount = NumberFilter(
+        field_name='bed_amount', lookup_expr='gte')
+    to_bedamount = NumberFilter(
+        field_name='bed_amount', lookup_expr='lte')
+
+    class Meta:
+        model = Room
+        fields = ['room_type', 'from_dayprice',
+                  'to_dayprice', 'from_bedamount', 'to_bedamount', 'room_number']
+
+
 class GenericRoomList(generics.ListCreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+    filter_class = RoomFilter
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['room_type',
+                     'day_price', 'bed_amount', 'room_number']
+    ordering_fields = ['room_type',
+                       'day_price', 'bed_amount']
 
 
 class Roomdetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = RoomSerializer
     queryset = Room.objects.all()
-    name='room-detail' 
+    name = 'room-detail'
+
+
+class ReservationFilter(FilterSet):
+    from_date = DateTimeFilter(
+        field_name='start_date', lookup_expr='gte')
+    to_date = DateTimeFilter(
+        field_name='end_date', lookup_expr='lte')
+
+    class Meta:
+        model = Reservation
+        fields = ['id_customer', 'id_room', 'from_date', 'to_date']
 
 
 class GenericReservationList(generics.ListCreateAPIView):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
 
-     
     def create(self, request, *args, **kwargs):
         data = request.data
         customer = Customer.objects.get(pk=data['id_customer'])
-        new_reservation = Reservation.objects.create(id_customer=customer,start_date=data['end_date'], end_date=data['end_date'])
+        new_reservation = Reservation.objects.create(
+            id_customer=customer, start_date=data['end_date'], end_date=data['end_date'])
         new_reservation.save()
         for id_room in data['id_room']:
             room = Room.objects.get(pk=id_room)
             new_reservation.id_room.add(room)
-         
+
         serializer = ReservationSerializer(new_reservation)
         return Response(serializer.data)
-
-
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['id_customer', 'id_room'] 
+    filter_class = ReservationFilter
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['id_customer', 'id_room']
     ordering_fields = ['id_customer', 'id_room', 'start_date', 'end_date']
 
@@ -126,8 +173,9 @@ class GenericReservationList(generics.ListCreateAPIView):
 class ReservationList(generics.ListAPIView):
     queryset = Reservation.objects.all()
     serializer_class = ReservationHyperlinkedSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['id_customer', 'id_room'] 
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['id_customer', 'id_room']
     search_fields = ['id_customer', 'id_room']
     ordering_fields = ['id_customer', 'id_room', 'start_date', 'end_date']
 
