901f0e64f1f79f15e90a52005d2be7dd15e62a3d
Piotr Martyniuk <piotras2137@gmail.com>
2022-01-20 18:17:51 +0100

    pep8

--- PSI/hotel/serializers.py
+++ PSI/hotel/serializers.py
@@ -1,22 +1,24 @@
 import re
-from rest_framework import fields, serializers
-from .models import *
 from datetime import *
+
+from rest_framework import fields, serializers
+
+from .models import Customer, Report, Reservation, Room
+
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = ['id', 'first_name', 'second_name', 'phone_number', 'email', 'personal_id']
-
+        fields = ['id', 'first_name', 'second_name',
+                  'phone_number', 'email', 'personal_id']
 
     def validate_email(self, value):
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
-        if not (re.fullmatch(regex,value)):
+        if not (re.fullmatch(regex, value)):
             raise serializers.ValidationError('email is not valid')
-        
-        return value
 
+        return value
 
     def create(self, validated_data):
         return Customer.objects.create(**validated_data)
@@ -27,17 +29,17 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = ['id', 'room_number', 'room_type', 'day_price', 'bed_amount']
 
-    
     def validate_day_price(self, value):
-        if value<=0:
-            raise serializers.ValidationError('day price should be higher than zero')
+        if value <= 0:
+            raise serializers.ValidationError(
+                'day price should be higher than zero')
 
         return value
 
-    
     def validate_bed_amount(self, value):
-        if value<1:
-            raise serializers.ValidationError('bed amount should be at least one')
+        if value < 1:
+            raise serializers.ValidationError(
+                'bed amount should be at least one')
 
         return value
 
@@ -54,6 +56,7 @@ class ReservationSerializer(serializers.ModelSerializer):
 class ReservationDetailSerializer(serializers.ModelSerializer):
     daycost = serializers.SerializerMethodField('full_price')
     totalcost = serializers.SerializerMethodField('whole_price')
+
     def full_price(self, reservation):
         rooms = reservation.id_room.all()
         total = 0
@@ -73,10 +76,10 @@ class ReservationDetailSerializer(serializers.ModelSerializer):
 
         return total * abs((d2 - d1).days)
 
-
     class Meta:
         model = Reservation
-        fields = ['id', 'id_customer', 'id_room', 'start_date', 'end_date','daycost', 'totalcost']
+        fields = ['id', 'id_customer', 'id_room',
+                  'start_date', 'end_date', 'daycost', 'totalcost']
 
 
 class ReservationHyperlinkedSerializer(serializers.HyperlinkedModelSerializer):
@@ -84,20 +87,20 @@ class ReservationHyperlinkedSerializer(serializers.HyperlinkedModelSerializer):
         many=False,
         read_only=True,
         slug_field='second_name'
-     )
+    )
 
-    id_room = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='room-detail')
-    
+    id_room = serializers.HyperlinkedRelatedField(
+        many=True, read_only=True, view_name='room-detail')
+
     class Meta:
         model = Reservation
-        fields = ['id', 'id_customer', 'id_room', 'start_date', 'end_date',]
+        fields = ['id', 'id_customer', 'id_room', 'start_date', 'end_date', ]
 
 
 class ReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Report
-        fields = ['id','reservations']
-
+        fields = ['id', 'reservations']
 
 
 class ReportDetailedSerializer(serializers.ModelSerializer):
@@ -111,7 +114,7 @@ class ReportDetailedSerializer(serializers.ModelSerializer):
     def reservationcount(self, report):
         return report.reservations.count()
 
-    def whole_price(self,reservation):
+    def whole_price(self, reservation):
         rooms = reservation.id_room.all()
         total = 0
         for i in rooms:
@@ -123,38 +126,37 @@ class ReportDetailedSerializer(serializers.ModelSerializer):
         return total * abs((d2 - d1).days)
 
     def totalprice(self, report):
-        totalprice = 0 
-        reservations = report.reservations.all() 
+        totalprice = 0
+        reservations = report.reservations.all()
         for i in reservations:
-            totalprice+=self.whole_price(i)
+            totalprice += self.whole_price(i)
         return totalprice
 
     def totalrooms(self, report):
-        totalrooms = 0 
-        reservations = report.reservations.all() 
+        totalrooms = 0
+        reservations = report.reservations.all()
         for i in reservations:
-            totalrooms+= i.id_room.count()
+            totalrooms += i.id_room.count()
         return totalrooms
 
     def avgrooms(self, report):
-        totalrooms = 0 
-        reservations = report.reservations.all() 
+        totalrooms = 0
+        reservations = report.reservations.all()
         for i in reservations:
-            totalrooms+= i.id_room.count()
+            totalrooms += i.id_room.count()
         return totalrooms / reservations.count()
-    
+
     def avgprice(self, report):
-        totalprice = 0 
-        reservations = report.reservations.all() 
+        totalprice = 0
+        reservations = report.reservations.all()
         for i in reservations:
-            totalprice+=self.whole_price(i)
+            totalprice += self.whole_price(i)
         return totalprice / report.reservations.count()
 
-
     def uniquecustomers(self, report):
-        uniquecustomer = 0 
+        uniquecustomer = 0
         customers = []
-        reservations = report.reservations.all() 
+        reservations = report.reservations.all()
         for i in reservations:
             customer = i.id_customer
             stop = False
@@ -163,10 +165,11 @@ class ReportDetailedSerializer(serializers.ModelSerializer):
                     stop = True
             if stop == False:
                 customers.append(customer)
-                uniquecustomer+=1
+                uniquecustomer += 1
 
         return uniquecustomer
 
     class Meta:
         model = Report
-        fields = ['id','owner', 'reservations', 'reservation_count', 'total_price', 'total_rooms',  'avg_price', 'avg_rooms', 'unique_customers']
+        fields = ['id', 'owner', 'reservations', 'reservation_count', 'total_price',
+                  'total_rooms',  'avg_price', 'avg_rooms', 'unique_customers']
